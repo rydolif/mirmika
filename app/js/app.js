bbcd11dfa9070f416cfcca69558e19c75d48c5a9
master <rudolifrudolif@gmail.com>
2020-09-24 14:56:46 +0300

    + scroll menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+//----------------------SLIDER-----------------------
 	var mySwiper = new Swiper('.hero__slider', {
 		loop: true,
 		pagination: {
@@ -11,5 +12,64 @@ document.addEventListener("DOMContentLoaded", function() {
 			prevEl: '.hero__prev',
 		},
 	});
+
+
+//----------------------MODAL-----------------------
+	const modals = (triggerSelector, modalSelector, closeSelector) => {
+		const trigger = document.querySelectorAll(triggerSelector),
+					modal = document.querySelector(modalSelector),
+					close = document.querySelector(closeSelector);
+
+		trigger.forEach(item => {
+			item.addEventListener('click', (e) => {
+				if (e.target) {
+					e.preventDefault();
+				}
+				modal.style.display = 'flex';
+				document.body.classList.add('modal__open')
+			});
+		})
+
+		close.addEventListener('click', () => {
+			modal.style.display = 'none';
+			document.body.classList.remove('modal__open');
+		});
+
+		modal.addEventListener('click', (e) => {
+			if (e.target === modal) {
+				modal.style.display = 'none';
+				document.body.classList.remove('modal__open');
+			}
+		});
+
+	};
+	modals('.order__open', '.modal__order', '.modal__order .modal__close');
+	modals('.click__open', '.modal__click', '.modal__click .modal__close');
+
+//----------------------SCROLL-----------------------
+	const scrollTo = (scrollTo) => {
+		document.querySelectorAll('a[href^="#"').forEach(link => {
+
+			link.addEventListener('click', function(e) {
+					e.preventDefault();
+					const scrollMenu = document.querySelector(scrollTo);
+	
+					let href = this.getAttribute('href').substring(1);
+	
+					const scrollTarget = document.getElementById(href);
+	
+					const topOffset = scrollMenu.offsetHeight;
+					const elementPosition = scrollTarget.getBoundingClientRect().top;
+					const offsetPosition = elementPosition - topOffset;
+	
+					window.scrollBy({
+							top: offsetPosition,
+							behavior: 'smooth'
+					});
+			});
+		});
+	};
+	scrollTo('.scrollto');
+
 
 });
